58294c3451dd3f98b5e94bc5a39a321b6e62d9b2
marukun_ <87289638+marukun712@users.noreply.github.com>
2022-12-13 15:07:40 +0900

    commit

--- comment.js
+++ comment.js
@@ -5,29 +5,46 @@ import fs from 'fs';
 
 var result = []
 
-async function getid() {
+async function GetId() {
     let res = await fetch(`https://www.googleapis.com/youtube/v3/playlistItems?key=${apikey}&playlistId=${list}&part=snippet&maxResults=50`)
     let json = await res.json();
     let items = await json.items
     let num = await items.length
+
     for (let i = 0; i < num; i++) {
-        var id = await items[i].snippet.resourceId.videoId
-        var videotitle = await items[i].snippet.title
-        var image = await items[i].snippet.thumbnails.default
-        var url = await `https://www.youtube.com/watch?v=${id}`
-        let res = await fetch(`https://www.googleapis.com/youtube/v3/commentThreads?key=${apikey}&part=snippet&videoId=${id}`)
-        let json = await res.json();
-        let num = json.items?.length
-        for (let i = 0; i < num; i++) {
-            var comment = await json.items[i].snippet.topLevelComment.snippet.textOriginal
-            var str = /[0-9]{1,}:[0-9]{1,}:[0-9]{1,}(.*)(.*)|[0-9]{1,}:[0-9]{1,}(.*)(.*)/gi
-            let body = await (String(comment).match(str));
-            let time = await (String(comment).match(/[0-9]{1,}:[0-9]{1,}:[0-9]{1,}|[0-9]{1,}:[0-9]{1,}/gi))
-            let title = await (String(body).replace(/[0-9]{1,}:[0-9]{1,}:[0-9]{1,}|[0-9]{1,}:[0-9]{1,}/gi, ""));
-            let titledata = title.split(',');
-            if (time?.length < 5) {
-            } else if (body === null) {
-            } else {
+        //動画ID、タイトル、サムネイルを取得する
+        let Id = await items[i].snippet.resourceId.videoId
+        let VideoTitle = await items[i].snippet.title
+        let Image = await items[i].snippet.thumbnails.default
+        await GetTimeStamp(
+            {
+                "Id": Id,
+                "Title": VideoTitle,
+                "Image": Image
+            }
+        )
+    }
+
+    fs.writeFile('data.json', JSON.stringify(result, null, '    '), (err) => {
+        if (err) console.log(`error!::${err}`);
+    });
+}
+
+async function GetTimeStamp(props) {
+    let res = await fetch(`https://www.googleapis.com/youtube/v3/commentThreads?key=${apikey}&part=snippet&videoId=${props.Id}`)
+    let json = await res.json();
+    let num = await json.items.length
+    let url = await `https://www.youtube.com/watch?v=${props.Id}`
+
+    for (let i = 0; i < num; i++) {
+        let comment = await json.items[i].snippet.topLevelComment.snippet.textOriginal
+        //コメントからタイムスタンプ部分を抜き出す
+        let body = await (String(comment).match(/[0-9]{1,}:[0-9]{1,}:[0-9]{1,}(.*)(.*)|[0-9]{1,}:[0-9]{1,}(.*)(.*)/gi));
+        let time = await (String(comment).match(/[0-9]{1,}:[0-9]{1,}:[0-9]{1,}|[0-9]{1,}:[0-9]{1,}/gi))
+        let title = await (String(body).replace(/[0-9]{1,}:[0-9]{1,}:[0-9]{1,}|[0-9]{1,}:[0-9]{1,}/gi, ""));
+        let titledata = await title.split(',');
+        try {
+            if (time.length > 5) {
                 let stampdata = []
                 for (let i = 0; i < time.length; i++) {
                     stampdata.push({
@@ -40,22 +57,21 @@ async function getid() {
                 result.push({
                     item: {
                         'timestamp': stampdata,
-                        'id': id,
-                        'videotitle': videotitle,
-                        'image': image,
+                        'id': props.Id,
+                        'videotitle': props.Title,
+                        'image': props.Image,
                         'url': url
                     }
                 })
                 break;
             }
-
+        }
+        catch {
+            console.log('Failed.')
         }
 
 
     }
-    fs.writeFile('data.json', JSON.stringify(result, null, '    '), (err) => {
-        if (err) console.log(`error!::${err}`);
-    });
 }
-getid();
+GetId();
 
