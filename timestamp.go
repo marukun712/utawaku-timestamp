d0fa603c59968144ed292bc7cf0c2aecd6c2eb1a
marukun712 <marukun530@gmail.com>
2023-11-07 16:25:49 +0900

    ✨タイムスタンプコメントを整形してJSONファイルへの書き出し機能を追加

--- timestamp.go
+++ timestamp.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"os"
 	"regexp"
 	"strings"
 
@@ -12,19 +13,22 @@ import (
 	"google.golang.org/api/youtube/v3"
 )
 
+// タイムスタンプオブジェクト
 type TimeStamp struct {
 	Time  string `json:"time"`
 	Title string `json:"title"`
 }
 
-type VideoDetails struct {
-	Items TimeStamp `json:"items"`
-	Title string    `json:"title"`
-	ID    string    `json:"id"`
-	Image string    `json:"image"`
+// 動画の詳細情報オブジェクト
+type VideoDetail struct {
+	TimeStamp []TimeStamp `json:"timeStamps"`
+	Title     string      `json:"title"`
+	ID        string      `json:"id"`
+	Image     string      `json:"image"`
 }
 
 func main() {
+	//Youtube Serviceの作成
 	ctx := context.Background()
 	youtubeService, err := youtube.NewService(ctx, option.WithAPIKey(""))
 
@@ -32,11 +36,24 @@ func main() {
 		log.Fatal(err)
 	}
 
+	//プレイリストIDからvideoIDを全て取得
 	videoIDs := fetchVideoIDs("PLhu18ozRJ5d3XLfoeUw6WQxGJAydvC-iL", youtubeService)
 
-	comments := fetchVideoComments(videoIDs, youtubeService)
+	//videoIDから動画の詳細情報とタイムスタンプを取得
+	details := fetchVideoDetails(videoIDs, youtubeService)
 
-	checkTimeStamp(comments)
+	//JSONファイルに書き込み
+	file, err := os.Create("data.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "  ")
+	if err := encoder.Encode(details); err != nil {
+		log.Fatal(err)
+	}
 }
 
 // プレイリストから動画IDをすべて取得
@@ -52,6 +69,7 @@ func fetchVideoIDs(playlistID string, service *youtube.Service) (videoIDs []stri
 		videoIDs = append(videoIDs, item.ContentDetails.VideoId)
 	}
 
+	//NextPageTokenがあれば次ページのデータも取得
 	if playlistsResponse.NextPageToken != "" {
 		nextPageToken := playlistsResponse.NextPageToken
 		for {
@@ -73,60 +91,90 @@ func fetchVideoIDs(playlistID string, service *youtube.Service) (videoIDs []stri
 	return videoIDs
 }
 
-// 動画IDからコメント情報を取得
-func fetchVideoComments(videoIDs []string, service *youtube.Service) (videoComments []string) {
+// 動画IDから詳細情報を取得
+func fetchVideoDetails(videoIDs []string, service *youtube.Service) (videoDetails []VideoDetail) {
 	for _, id := range videoIDs {
-		videoCommentsCall := service.CommentThreads.List([]string{"id", "snippet"}).VideoId(id).MaxResults(100)
+		var videoComments []string
+
+		//動画ごとにコメントを取得
+		videoCommentsCall := service.CommentThreads.List([]string{"id", "snippet"}).VideoId(id).MaxResults(100).Order("relevance")
 
 		videoCommentsResponse, err := videoCommentsCall.Do()
 		if err != nil {
 			log.Fatalf("Error call YouTube API: %v", err)
 		}
 
+		//動画の詳細情報を取得
+		videosCall := service.Videos.List([]string{"snippet", "statistics"}).Id(id)
+		response, err := videosCall.Do()
+		if err != nil {
+			log.Fatalf("Error call YouTube API: %v", err)
+		}
+		item := response.Items[0]
+
+		//動画タイトル、サムネイルURL
+		title := item.Snippet.Title
+		image := "https://img.youtube.com/vi/" + id + "/maxresdefault.jpg"
+
 		for _, item := range videoCommentsResponse.Items {
 			text := item.Snippet.TopLevelComment.Snippet.TextOriginal
 
 			videoComments = append(videoComments, text)
 		}
+
+		//タイムスタンプコメントを抽出
+		timeStamp := getTimeStamp(videoComments)
+
+		//VideoDetailオブジェクトの作成
+		d := VideoDetail{timeStamp, title, id, image}
+
+		videoDetails = append(videoDetails, d)
 	}
 
-	return videoComments
+	return videoDetails
 }
 
-func checkTimeStamp(videoComments []string) {
+// コメントから正規表現でタイムスタンプコメントを抽出
+func getTimeStamp(videoComments []string) (timeStamps []TimeStamp) {
 	for _, item := range videoComments {
+		//タイムスタンプコメントの全文を取得
 		pattern := `[0-9]{1,}:[0-9]{1,}:[0-9]{1,}(.*)|[0-9]{1,}:[0-9]{1,}(.*)`
 
-		// 正規表現をコンパイル
+		//コンパイル
 		re, err := regexp.Compile(pattern)
 		if err != nil {
 			fmt.Println("Error compiling regex:", err)
-			return
 		}
 
-		// マッチングを行う
+		//マッチング
 		matches := re.FindAllString(item, -1)
 
+		//タイムスタンプが5件以上なら
 		if len(matches) > 5 {
 			for _, item := range matches {
+				//タイムスタンプのみを取得
 				pattern := `[0-9]{1,}:[0-9]{1,}:[0-9]{1,}|[0-9]{1,}:[0-9]{1,}`
 
+				//コンパイル
 				re, err := regexp.Compile(pattern)
 				if err != nil {
 					fmt.Println("Error compiling regex:", err)
-					return
 				}
-				timestamp := re.FindAllString(item, -1)
+				//マッチング
+				time := re.FindAllString(item, -1)
+
+				//タイムスタンプ部分を削除してタイトルを取得
 				title := re.ReplaceAllString(item, "")
 
-				t := TimeStamp{strings.TrimSpace(timestamp[0]), strings.TrimSpace(title)}
-				bytes, err := json.Marshal(t)
-				if err != nil {
-					log.Fatal(err)
-				}
+				//TimeStampオブジェクトの作成
+				t := TimeStamp{strings.TrimSpace(time[0]), strings.TrimSpace(title)}
 
-				fmt.Println(string(bytes))
+				timeStamps = append(timeStamps, t)
 			}
+
+			break
 		}
 	}
+
+	return timeStamps
 }
